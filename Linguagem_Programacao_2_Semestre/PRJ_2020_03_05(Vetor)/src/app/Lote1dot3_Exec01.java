171ef86d7c037be01934a838b4d4036482ab3d84
fernando <fernansoft111@gmail.com>
2020-03-18 19:13:51 -0300

    Inserção da função fSomaImpares, para atender o requisito "b". Alteração de "int" para "double" na variável "recebeSomaF1", para mostrar a média em decimal.

--- Linguagem_Programacao_2_Semestre/PRJ_2020_03_05(Vetor)/src/app/Lote1dot3_Exec01.java
+++ Linguagem_Programacao_2_Semestre/PRJ_2020_03_05(Vetor)/src/app/Lote1dot3_Exec01.java
@@ -4,7 +4,7 @@ Objetivo   : Exec01. Criar e coletar um vetor [50] inteiro.
              a. A média dos valores entre 10 e 200;
              b. A soma dos números ímpares. 
 Programador: Fernando Oliveira da Costa
-Data       : 15/03/2020
+Data       : 18/03/2020
 */
 package app;
 
@@ -25,22 +25,35 @@ public class Lote1dot3_Exec01
             vetorExec01[i] = random.nextInt(500) + 1;
         }
         System.out.println("Vetor gerado com valores aleatórios:\n"+ Arrays.toString(vetorExec01));
-        fMediaValores(vetorExec01);
+        fMediaValoresF1(vetorExec01);
+        fSomaImpares(vetorExec01);
     }
 
 
-    public static void fMediaValores(int n[])
-    //fMediaValores: Calcula a média dos valores entre 10 e 200 do vetor.
+    public static void fMediaValoresF1(int n[])
+    //fMediaValores: Calcula e mostra a média dos valores entre 10 e 200 do vetor.
     {
-        int recebeSoma = 0, recebeQuantidade = 0;
+        double recebeSomaF1 = 0, recebeQuantidadeF1 = 0;
         for(int i=0; i<(n.length); i++)
         {
             if(n[i]>10 && n[i]<200)
             {
-                recebeSoma += n[i];
-                recebeQuantidade += 1;
+                recebeSomaF1 += n[i];
+                recebeQuantidadeF1 += 1;
             }
         }
-        JOptionPane.showMessageDialog(null, "A média dos valores entre 10 e 200 do vetor é de: "+ (recebeSoma/recebeQuantidade));
+        JOptionPane.showMessageDialog(null, "A média dos valores entre 10 e 200 do vetor é de: "+ (recebeSomaF1/recebeQuantidadeF1));
+    }
+
+    public static void fSomaImpares(int n[])
+    //fSomaImpares: Soma e mostra os valores de todos os números ímpares do vetor.
+    {
+        int recebeSomaF2 = 0;
+        for(int i=0; i<(n.length); i++)
+        {
+            if(n[i] % 2 != 0)
+            {recebeSomaF2 += n[i];}
+        }
+        JOptionPane.showMessageDialog(null, "A soma dos valores ímpares dentro do vetor gerado é: "+ recebeSomaF2);
     }
 }
