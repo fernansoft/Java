180a8f01dc5b5a90c309b30af931e1b1f241fe2c
Fernando Oliveira <fernansoft111@gmail.com>
2021-05-26 13:58:07 -0300

    Melhoria na funcionalidade
    Foi substituído o MouseEvent pelo ActionEvent, assim mesmo o pressionamento com o teclado aciona a função.

--- ProgOrientadaAObj/17-EventsBotãoHello/src/EventBotaoHello/JanelaComBotaoHello.java
+++ ProgOrientadaAObj/17-EventsBotãoHello/src/EventBotaoHello/JanelaComBotaoHello.java
@@ -1,25 +1,25 @@
 package EventBotaoHello;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class JanelaComBotaoHello extends Application {
 
-    class AssinanteMouse implements EventHandler<MouseEvent> {
-        public void handle(MouseEvent e) {
+    static class AssinanteMouse implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent e) {
             System.out.println("Hello World!");
         }
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         BorderPane bp = new BorderPane();
         Scene scn = new Scene(bp, 600, 400);
 
@@ -28,7 +28,7 @@ public class JanelaComBotaoHello extends Application {
 
         AssinanteMouse a = new AssinanteMouse();
 
-        btn.addEventFilter(MouseEvent.MOUSE_PRESSED, a);
+        btn.addEventFilter(ActionEvent.ACTION, a);
 
         BorderPane.setAlignment(btn, Pos.CENTER);
         BorderPane.setAlignment(lbl, Pos.CENTER);
