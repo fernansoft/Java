bafab220087b7455691ade5f4cccf270d6fee5b4
Fernando Oliveira <fernansoft111@gmail.com>
2021-06-02 16:10:38 -0300

    Não se trata de um CRUD completo

--- ProgOrientadaAObj/19-TestPadraoBCE/src/AlunosBCE/BoundaryAluno.java
+++ ProgOrientadaAObj/19-TestPadraoBCE/src/AlunosBCE/BoundaryAluno.java
@@ -1,4 +1,4 @@
-package CRUDAlunosBCE;
+package AlunosBCE;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
--- ProgOrientadaAObj/19-TestPadraoBCE/src/AlunosBCE/EntityAluno.java
+++ ProgOrientadaAObj/19-TestPadraoBCE/src/AlunosBCE/EntityAluno.java
@@ -1,4 +1,4 @@
-package CRUDAlunosBCE;
+package AlunosBCE;
 
 import java.util.Date;
 
