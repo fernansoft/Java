e48bd4bb710e2227430096b0d8bd627dd0f2333d
Fernando Oliveira <fernansoft111@gmail.com>
2021-06-02 19:05:37 -0300

    Mais um teste BCE
    Este um pouco mais organizado.

---
+++ ProgOrientadaAObj/19-TestPadraoBCE/src/LivrosBCE/LivroBCE/LivroControl.java
@@ -0,0 +1,21 @@
+package LivrosBCE.LivroBCE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LivroControl {
+    private List<Livro> livros = new ArrayList<>();
+
+    public void adicionar(Livro l) {
+        livros.add(l);
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        for (Livro l : livros) {
+            if(l.getTitulo().contains(titulo)){
+                return l;
+            }
+        }
+        return null;
+    }
+}
