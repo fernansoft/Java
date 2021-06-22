856df6ec0740540c911300c665cd5e63b86101aa
Fernando Oliveira <fernansoft111@gmail.com>
2021-06-22 11:29:21 -0300

    Revert "ProjetoFarmacia"
    This reverts commit 24b4e2770471359b460bf6de0334a12b763fff7a.

--- ProgOrientadaAObj/21-ProjetoFarmacia/src/Farmacia/DAOAtendente.java
+++
@@ -1,9 +0,0 @@
-package Farmacia;
-
-import java.util.List;
-
-public interface DAOAtendente {
-    void cadastrar(AtendenteEntity a);
-    boolean confAtends(String nome);
-    boolean confLog(String nome, String pass);
-}
--- ProgOrientadaAObj/21-ProjetoFarmacia/src/Farmacia/DAOMedicamento.java
+++
@@ -1,9 +0,0 @@
-package Farmacia;
-
-import java.util.List;
-
-public interface DAOMedicamento {
-    void adicionar(MedicamentoEntity m);
-    void remover(String id);
-    List<MedicamentoEntity> pesquisarPorNome(String nome);
-}
